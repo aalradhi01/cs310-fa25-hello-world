68f31ba6118e48dc555e85cc559f1af9c48c5bad
Ali Alradhi <aalradhi01@gmail.com>
2025-09-02 13:32:05 -0500

    Add f2() to Class1, Class2, HelloWorld

--- Class2.java
+++ Class2.java
@@ -8,5 +8,9 @@ public class Class2 {
     private void f1() {
         // v1.0
     }
+      public void f2() {
+    System.out.println("f2");
+}
+
 
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -15,5 +15,8 @@ public class HelloWorld {
     private void f1() {
         // v1.0
     }
+      public void f2() {
+    System.out.println("f2");
+}
 
 }
