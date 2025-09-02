f5209ea367b09ee41476fce8cc07c8e5b97b7353
Ali Alradhi <aalradhi01@gmail.com>
2025-09-02 13:30:53 -0500

    Set author tags; fix HelloWorld types (Class→Class1/Class2)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,14 @@
 /**
  *
- * @author Your Name
+ * @author Ali Alradhi
  */
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
+        Class1 c1 = new Class1();
+        Class2 c2 = new Class2();
+
         System.out.println("Hello, World!");
     }
     
